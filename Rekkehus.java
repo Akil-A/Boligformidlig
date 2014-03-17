de58e1e3e43f0b62aef429c672ca00750038e124
A-Lee <s198734@stud.hioa.no>
2014-03-17 16:40:59 +0100

    Create Rekkehus.java

---
+++ Rekkehus.java
@@ -0,0 +1,27 @@
+public class Rekkehus extends Bolig
+{
+	private int antetasjer;
+	private boolean kjeller;
+	private int tomtestr;
+	
+	public Rekkehus(int antetasjer, boolean kjeller, int tomtestr,String adresse,String boligtype, int boareal, int antrom,String byggeaar,String beskrivelse, String utleiepris, int aar, int maaned, int dag)
+	{
+	super(adresse,boligtype,boareal,antrom,byggeaar,beskrivelse,utleiepris,aar,maaned,dag);	
+	
+	this.antetasjer = antetasjer;
+	this.kjeller = kjeller;
+	this.tomtestr = tomtestr;
+	}
+public int getAntetasjer()
+{
+	return antetasjer;
+}
+public boolean getKjeller()
+{
+	return kjeller;
+}
+public int getTomtestr()
+{
+	return tomtestr;
+}
+}
