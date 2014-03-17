1b9001515d946283e695930463b5a0c0a4edb98e
A-Lee <s198734@stud.hioa.no>
2014-03-17 17:23:54 +0100

    Update Enebolig.java

--- Enebolig.java
+++ Enebolig.java
@@ -4,7 +4,7 @@ public class Enebolig extends Bolig
 	private boolean kjeller;
 	private int tomtestr;
 	
-	public Enebolig(int antetasjer, boolean kjeller, int tomtestr,String adresse, int boareal, int antrom,String byggeaar,String beskrivelse, String utleiepris, int aar, int maaned, int dag)
+	public Enebolig(int antetasjer, boolean kjeller, int tomtestr,String adresse, int boareal, int antrom,String byggeaar,String beskrivelse, int utleiepris, int aar, int maaned, int dag)
 	{
 	super(adresse,boareal,antrom,byggeaar,beskrivelse,utleiepris,aar,maaned,dag);	
 	
