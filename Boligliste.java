cb57be0b4922d66f51105291fc6d513d8f544967
joakim1123 <joakimm89@gmail.com>
2014-03-17 18:18:56 +0100

    Update Boligliste.java

--- Boligliste.java
+++ Boligliste.java
@@ -14,7 +14,9 @@ public class Boligliste
 		boliger.remove(indeks);
 	}
 	
-	public Boligliste finnBolig(String adresse, int boareal, int antrom, String byggeaar, int utleiepris)
+	public Boligliste finnBolig(String adresse, int boareal, int antrom, String byggeaar, int utleiepris,
+			int antetasjer, boolean kjeller, int tomtestr,
+			int etasje, boolean heis, boolean balkong)
 	{
 		List<Bolig> bl = new ArrayList<>();
 		
@@ -22,13 +24,29 @@ public class Boligliste
 		
 		while (iter.hasNext())
 		{
-			if (iter.getAdresse() == adresse || iter.getBoareal() == boareal || iter.getAntrom() == antrom ||
-					iter.getByggeaar() == byggeaar || iter.getUtleiepris() == utleiepris)
+			if (iter.getAdresse().equals(adresse) || iter.getBoareal() == boareal || iter.getAntrom() == antrom ||
+					iter.getByggeaar().equals(byggeaar) || iter.getUtleiepris() == utleiepris ||
+					((iter instanceof Enebolig || iter instanceof Rekkehus) && (iter.getAntetasjer() == antetasjer || iter.getKjeller() == kjeller || iter.getTomtestr() == tomtestr)) ||
+					(iter instanceof Leilighet && (iter.getEtasje() == etasje || iter.isHeis() == heis || iter.isBalkong() == balkong)))
 				bl.add(iter.next());
 			else
 				iter.next();
 		}
 		
 		return bl;
+	}
+	
+	public String visListe()
+	{
+		Iterator<Bolig> iter = boliger.iterator();
+		
+		String s = "";
+		
+		while (iter.hasNext())
+		{
+			s += iter.next().toString() + "\n";
+		}
+		
+		return s;
 	}
 }
