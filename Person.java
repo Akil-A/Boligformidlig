466284380a8699389126ff0345d260959ed062c6
Akil-A <mail@asghara.com>
2014-03-17 17:27:03 +0100

    Update Person.java

--- Person.java
+++ Person.java
@@ -13,37 +13,33 @@ public class Person {
 		this.email = email;
 		this.telefon = telefon;
 	}
-	
-	public String getNavn()
-	{
+
+	public String getNavn() {
 		return navn;
-	}
-	
-	public String getAdresse()
-	{
-		return adresse;
-	}
-	
-	public String getEmail()
-	{
-		return email;
-	}
-	
-	public String getTelefon()
-	{
-		return telefon;
 	}
 
 	public void setNavn(String navn) {
 		this.navn = navn;
 	}
 
+	public String getAdresse() {
+		return adresse;
+	}
+
 	public void setAdresse(String adresse) {
 		this.adresse = adresse;
 	}
 
+	public String getEmail() {
+		return email;
+	}
+
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public String getTelefon() {
+		return telefon;
 	}
 
 	public void setTelefon(String telefon) {
