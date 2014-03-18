e5c70556e064af3443da8f7dd011f31ac1c8a88c
A-Lee <s198734@stud.hioa.no>
2014-03-18 11:56:50 +0100

    Update Kontraktliste.java

--- Kontraktliste.java
+++ Kontraktliste.java
@@ -1,42 +1,47 @@
 import java.util.*;
-public class Kontraktliste 
+
+public class Kontraktliste
+
 {
+	List<Kontrakt> kontrakter = new ArrayList<>();
 	
-		List<Kontrakt> kontrakter = new ArrayList<>();
-		
-		public void settInnKontrakt(Kontrakt k)
-		{
-			kontrakter.add(k);
-		}
-		
-		public void fjernKontrakt (int indeks)
-		{
-			kontrakter.remove(indeks);
-		}
-		
-		public Kontrakt finnKontrakt(String navn, String adresse)
-		{
-			ListIterator<Kontrakt> iter = kontrakter.listIterator();
-		
-			while(iter.hasNext())
-			{
-				if (iter.next().getNavn().equals(navn) || iter.next().getAdresse().equals(adresse) )
-				{
-					iter.add(iter.next());
-				}
-				else 
-					iter.next();
-			}
-			return iter.next();
-		}
-		
-		public String visKontrakt()
-		{
-			ListIterator<Kontrakt> iter = kontrakter.listIterator();
-			
-			String s = "";
-			
-			
-		}
+public void settInnKontrakt(Kontrakt k)
+
+{
+	kontrakter.add(k);
 }
 
+public void slettKontrakt(int indeks)
+
+{
+	kontrakter.remove(indeks);
+}
+
+public Kontrakt finnKontrakt(Utleier utleier, Boligsoker leietaker, String adresse, String telefon)
+
+{
+	ListIterator<Kontrakt> iter = kontrakter.listIterator();
+
+	while(iter.hasNext())
+	{
+		if(iter.next().getUtleier().equals(utleier) && iter.next().getLeietaker().equals(leietaker))
+		{
+			iter.add(iter.next());
+		}
+		else
+			iter.next();
+	}
+	return iter.next();
+}
+
+public String visKontrakt()
+
+{
+	ListIterator<Kontrakt> iter = kontrakter.listIterator();
+	String s = "";
+	s += iter.next().toString();
+	return s;
+}
+
+	
+}
