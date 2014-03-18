5e13f9714132564427a2cf0e8c64d81e38bae05f
joakim1123 <joakimm89@gmail.com>
2014-03-18 23:20:58 +0100

    Update Personliste.java

--- Personliste.java
+++ Personliste.java
@@ -6,38 +6,38 @@ public class Personliste
 	List<Person> personer = new ArrayList<>();
 
 
-public void settInnPerson(Person p)
-{
-	personer.add(p);
-}
-
-public void fjernPerson(int indeks)
-{
-	personer.remove(indeks);
-}
-
-public Person finnPerson(String fornavn, String etternavn, String telefon)
-{
-	ListIterator<Person> iter = personer.listIterator();
-	
-while(iter.hasNext())
+	public void settInnPerson(Person p)
 	{
-		if(iter.next().getFornavn.equals(fornavn) && iter.next()getEtternavn.equals(etternavn) && iter.next().getTelefon().equals(telefon))
-		{
-			iter.add(iter.next());
-		}
-		else
-			iter.next();
+		personer.add(p);
 	}
-	return iter.next();
-}
-
-public String visPerson()
-{
-	ListIterator<Person> iter = personer.listIterator();
-	String s = "";
-	s += iter.next().toString();
-	return s;
-}
+	
+	public void fjernPerson(int indeks)
+	{
+		personer.remove(indeks);
+	}
+	
+	public Person finnPerson(String fornavn, String etternavn, String telefon)
+	{
+		ListIterator<Person> iter = personer.listIterator();
+		
+	while(iter.hasNext())
+		{
+			if(iter.next().getFornavn().equals(fornavn) && iter.next().getEtternavn().equals(etternavn) && iter.next().getTelefon().equals(telefon))
+			{
+				iter.add(iter.next());
+			}
+			else
+				iter.next();
+		}
+		return iter.next();
+	}
+	
+	public String visPerson()
+	{
+		ListIterator<Person> iter = personer.listIterator();
+		String s = "";
+		s += iter.next().toString();
+		return s;
+	}
 
 }
