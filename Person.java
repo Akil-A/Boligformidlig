533f14901d701e25cbb195ce9425412f1755bf41
joakim1123 <joakimm89@gmail.com>
2014-03-18 12:49:40 +0100

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,78 +1,102 @@
-public class Person {
-
-private String fornavn;
-private String etternavn;
-private String adresse;
-private String email;
-private String telefon;
-private String poststed;
-private int postnr;
-
-public Person(String fornavn, String etternavn, String adresse, String poststed, int postnr, String email, String telefon)
+public class Person
 {
-this.fornavn = fornavn;
-this.etternavn = etternavn;
-this.adresse = adresse;
-this.email = email;
-this.telefon = telefon;
-this.poststed = poststed;
-this.postnr = postnr;
-}
-
-public String getFornavn() {
-return fornavn;
-}
-
-public void setFornavn(String fornavn) {
-this.fornavn = fornavn;
-}
-public String getEtternavn()
-{
-	return etternavn;
-}
-
-public void setEtternavn(String etternavn)
-{
-	this.etternavn = etternavn;
-}
-public String getAdresse() {
-return adresse;
-}
-
-public void setAdresse(String adresse) {
-this.adresse = adresse;
-}
-
-public String getEmail() {
-return email;
-}
-
-public void setEmail(String email) {
-this.email = email;
-}
-
-public String getTelefon() {
-return telefon;
-}
-
-public void setTelefon(String telefon) {
-this.telefon = telefon;
-}
-
-public String getPoststed() {
-	return poststed;
-}
-
-public void setPoststed(String poststed) {
-	this.poststed = poststed;
-}
-
-public int getPostnr() {
-	return postnr;
-}
-
-public void setPostnr(int postnr) {
-	this.postnr = postnr;
-}
+	private int personNr;
+	private String fornavn;
+	private String etternavn;
+	private String adresse;
+	private String email;
+	private String telefon;
+	private String poststed;
+	private int postnr;
+	
+	private static int personTeller = 0;
+	
+	public Person(String fornavn, String etternavn, String adresse, String poststed, int postnr, String email, String telefon)
+	{
+		this.fornavn = fornavn;
+		this.etternavn = etternavn;
+		this.adresse = adresse;
+		this.email = email;
+		this.telefon = telefon;
+		this.poststed = poststed;
+		this.postnr = postnr;
+		
+		personNr = personTeller++;
+	}
+	
+	public int getPersonNr()
+	{
+		return personNr;
+	}
+	
+	public String getFornavn()
+	{
+		return fornavn;
+	}
+	
+	public void setFornavn(String fornavn)
+	{
+		this.fornavn = fornavn;
+	}
+	
+	public String getEtternavn()
+	{
+		return etternavn;
+	}
+	
+	public void setEtternavn(String etternavn)
+	{
+		this.etternavn = etternavn;
+	}
+	
+	public String getAdresse()
+	{
+		return adresse;
+	}
+	
+	public void setAdresse(String adresse)
+	{
+		this.adresse = adresse;
+	}
+	
+	public String getEmail()
+	{
+		return email;
+	}
+	
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
+	
+	public String getTelefon()
+	{
+		return telefon;
+	}
+	
+	public void setTelefon(String telefon)
+	{
+		this.telefon = telefon;
+	}
+	
+	public String getPoststed()
+	{
+		return poststed;
+	}
+	
+	public void setPoststed(String poststed)
+	{
+		this.poststed = poststed;
+	}
+	
+	public int getPostnr()
+	{
+		return postnr;
+	}
+	
+	public void setPostnr(int postnr)
+	{
+		this.postnr = postnr;
+	}
 
 }
