20d3a7321edd469ce51fd2278fb25a3e649e1dde
A-Lee <s198734@stud.hioa.no>
2014-03-18 12:21:48 +0100

    Update Rekkehus.java

--- Rekkehus.java
+++ Rekkehus.java
@@ -1,49 +1,63 @@
 public class Rekkehus extends Bolig
 {
-	private int antetasjer;
-	private boolean kjeller;
-	private int tomtestr;
+		private int antetasjer;
+		private boolean kjeller;
+		private int tomtestr;
 	
-	public Rekkehus(int antetasjer, boolean kjeller, int tomtestr,String adresse, int boareal, int antrom,String byggeaar,String beskrivelse, int utleiepris, int aar, int maaned, int dag)
+	public Rekkehus(int antetasjer, boolean kjeller, int tomtestr,String adresse, int postnr, String poststed, int boareal, int antrom,String byggeaar,String beskrivelse, int utleiepris, int aar, int maaned, int dag)
 	{
-	super(adresse,boareal,antrom,byggeaar,beskrivelse,utleiepris,aar,maaned,dag);	
 	
-	this.antetasjer = antetasjer;
-	this.kjeller = kjeller;
-	this.tomtestr = tomtestr;
+		super(adresse, postnr, poststed, boareal,antrom,byggeaar,beskrivelse,utleiepris,aar,maaned,dag);	
+	
+			this.antetasjer = antetasjer;
+			this.kjeller = kjeller;
+			this.tomtestr = tomtestr;
 	}
-
-	public int getAntetasjer() {
+	
+	public int getAntetasjer() 
+	
+	{
 		return antetasjer;
 	}
-
-	public void setAntetasjer(int antetasjer) {
+	
+	public void setAntetasjer(int antetasjer) 
+	
+	{
 		this.antetasjer = antetasjer;
 	}
-
-	public boolean isKjeller() {
+	
+	public boolean isKjeller() 
+	
+	{
 		return kjeller;
 	}
-
-	public void setKjeller(boolean kjeller) {
+	
+	public void setKjeller(boolean kjeller) 
+	
+	{
 		this.kjeller = kjeller;
 	}
-
-	public int getTomtestr() {
+	
+	public int getTomtestr() 
+	
+	{
 		return tomtestr;
 	}
-
-	public void setTomtestr(int tomtestr) {
+	
+	public void setTomtestr(int tomtestr) 
+	
+	{
 		this.tomtestr = tomtestr;
 	}
 	
 	
 	public String toString()
+	
 	{
 		return super.toString() +
-		"\nAntall etasjer: " + antetasjer +
-		"\nKjeller: " + (kjeller ? "ja" : "nei") +
-		"\nTomtestr.: " + tomtestr + " kvm";
+			"\nAntall etasjer: " + antetasjer +
+			"\nKjeller: " + (kjeller ? "ja" : "nei") +
+			"\nTomtestr.: " + tomtestr + " kvm";
 	}
 
 }
