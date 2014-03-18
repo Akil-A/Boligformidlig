39c5361259312dd1460e1d2a47f2e69ce18413d0
A-Lee <s198734@stud.hioa.no>
2014-03-18 11:15:29 +0100

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,48 +1,79 @@
 
 public class Person {
-	
-	private String navn;
-	private String adresse;
-	private String email;
-	private String telefon;
-	
-	public Person(String navn, String adresse, String email, String telefon)
-	{
-		this.navn = navn;
-		this.adresse = adresse;
-		this.email = email;
-		this.telefon = telefon;
-	}
 
-	public String getNavn() {
-		return navn;
-	}
+private String fornavn;
+private String etternavn;
+private String adresse;
+private String email;
+private String telefon;
+private String poststed;
+private int postnr;
 
-	public void setNavn(String navn) {
-		this.navn = navn;
-	}
+public Person(String fornavn, String etternavn, String adresse, String email, String telefon, String poststed, int postnr)
+{
+this.fornavn = fornavn;
+this.etternavn = etternavn;
+this.adresse = adresse;
+this.email = email;
+this.telefon = telefon;
+this.poststed = poststed;
+this.postnr = postnr;
+}
 
-	public String getAdresse() {
-		return adresse;
-	}
+public String getFornavn() {
+return fornavn;
+}
 
-	public void setAdresse(String adresse) {
-		this.adresse = adresse;
-	}
+public void setFornavn(String fornavn) {
+this.fornavn = fornavn;
+}
+public String getEtternavn()
+{
+	return etternavn;
+}
 
-	public String getEmail() {
-		return email;
-	}
+public void setEtternavn(String etternavn)
+{
+	this.etternavn = etternavn;
+}
+public String getAdresse() {
+return adresse;
+}
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+public void setAdresse(String adresse) {
+this.adresse = adresse;
+}
 
-	public String getTelefon() {
-		return telefon;
-	}
+public String getEmail() {
+return email;
+}
 
-	public void setTelefon(String telefon) {
-		this.telefon = telefon;
-	}
+public void setEmail(String email) {
+this.email = email;
+}
+
+public String getTelefon() {
+return telefon;
+}
+
+public void setTelefon(String telefon) {
+this.telefon = telefon;
+}
+
+public String getPoststed() {
+	return poststed;
+}
+
+public void setPoststed(String poststed) {
+	this.poststed = poststed;
+}
+
+public int getPostnr() {
+	return postnr;
+}
+
+public void setPostnr(int postnr) {
+	this.postnr = postnr;
+}
+
 }
