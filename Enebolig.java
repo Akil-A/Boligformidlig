30647d574373cf2268bf3ff07d5dcb3e9bec8a25
A-Lee <s198734@stud.hioa.no>
2014-03-18 12:18:56 +0100

    Update Enebolig.java

--- Enebolig.java
+++ Enebolig.java
@@ -1,51 +1,59 @@
 public class Enebolig extends Bolig
 {
-private int antetasjer;
-private boolean kjeller;
-private int tomtestr;
+	private int antetasjer;
+	private boolean kjeller;
+	private int tomtestr;
 
 public Enebolig(int antetasjer, boolean kjeller, int tomtestr,String adresse, int postnr, String poststed, int boareal, int antrom,String byggeaar,String beskrivelse, int utleiepris, int aar, int maaned, int dag)
 {
-super(adresse, postnr, poststed, boareal,antrom,byggeaar,beskrivelse,utleiepris,aar,maaned,dag);	
+	super(adresse, postnr, poststed, boareal,antrom,byggeaar,beskrivelse,utleiepris,aar,maaned,dag);	
 
-this.antetasjer = antetasjer;
-this.kjeller = kjeller;
-this.tomtestr = tomtestr;
+		this.antetasjer = antetasjer;
+		this.kjeller = kjeller;
+		this.tomtestr = tomtestr;
 }
 
 public void setAntetasjer(int antetasjer)
+
 {
-this.antetasjer = antetasjer;
+	this.antetasjer = antetasjer;
 }
 public int getAntetasjer()
+
 {
-return antetasjer;
+	return antetasjer;
 }
 
 public void setKjeller(boolean kjeller)
+
 {
-this.kjeller = kjeller;
+	this.kjeller = kjeller;
 }
 
-public boolean getKjeller() {
-return kjeller;
+public boolean getKjeller() 
+
+{
+	return kjeller;
 }
 
 public void setTomtestr(int tomtestr)
+
 {
-this.tomtestr = tomtestr;
+	this.tomtestr = tomtestr;
 }
-public int getTomtestr() {
-return tomtestr;
+
+public int getTomtestr() 
+{
+	return tomtestr;
 }
 
 
 public String toString()
 {
-return super.toString() +
-"\nAntall etasjer: " + antetasjer +
-"\nKjeller: " + (kjeller ? "ja" : "nei") +
-"\nTomtestr.: " + tomtestr + " kvm";
+	return super.toString() +
+			"\nAntall etasjer: " + antetasjer +
+			"\nKjeller: " + (kjeller ? "ja" : "nei") +
+			"\nTomtestr.: " + tomtestr + " kvm";
 }
 
 
