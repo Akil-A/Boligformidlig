6e7df02e8017e0f2a2df317d1414c1065ee2877b
Akil-A <mail@asghara.com>
2014-03-18 12:57:09 +0100

    Create Personvindu.java

---
+++ Personvindu.java
@@ -0,0 +1,28 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+	
+public class Personvindu {
+
+	private JLabel felttekst1;
+	private JLabel felttekst2;
+
+	public Personvindu()
+	{	
+	  super("Personvindu");
+	  	
+	  	felttekst1 = new JLabel("Utleiere");
+	  	felttekst2 = new JLabel("Boligsokere");
+
+
+	    Container c = getContentPane();
+	    c.setLayout( new FlowLayout() );
+
+	    c.add( felttekst1);
+	    c.add( felttekst2);
+
+	    setSize( 420, 800 );
+	    setVisible( true );
+	    setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);   
+	  }
+}
