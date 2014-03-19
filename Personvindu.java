06db6ab832d867eb9f1b2f83b1e2a622b3fa7db8
Akil-A <mail@asghara.com>
2014-03-19 14:14:34 +0100

    Update Personvindu.java

--- Personvindu.java
+++ Personvindu.java
@@ -7,11 +7,16 @@ public class Personvindu extends JFrame
 
 	private JLabel felttekst1;
 	private JLabel felttekst2;
+	private JTextArea tekstomraade;
+	private JButton knapp;
 
-	public Personvindu()
+
+	public Personvindu(Boligsystem bs)
 	{	
 		super("Personvindu");
 		
+	    tekstomraade = new JTextArea();
+
 		felttekst1 = new JLabel("Utleiere");
 		felttekst2 = new JLabel("Boligsokere");
 		
@@ -20,15 +25,45 @@ public class Personvindu extends JFrame
 		
 		c.add( felttekst1);
 		c.add( felttekst2);
+		c.add(tekstomraade);
+		c.add(knapp);
 		
 		setSize( 420, 800 );
 		setVisible( true );
 		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);   
 		
-		list = new JList(//arraylist.toArray());
+		Personliste personer = bs.getPersoner();
+
+		list = new JList(personer.toArray());
 		list.setVisibleRowcount(10);
 		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		add(new JScrollPane(list));
 		list.addListSelectionListener( new ListSelectionListener()
+		
+				
+		public void valueChanged( ListSelectionEvent e)
+		{
+	         if ( !e.getValueIsAdjusting() )
+	         {
+	        	 JList<Person> liste = (JList) e.getSource();
+	        	 Person person = liste.getSelectedValue();
+	        	 String info = person.toString();
+	        	 tekstomraade.append(info);
+	         }
+		}
+		
+		   private class lytter implements ActionListener
+		   {
+		        public void actionPerformed( ActionEvent e )
+		        {
+		        	if(e.getsource() == knapp)
+		        	{
+		        		Personskjemavindu pv = new Personskjemavindu(liste.getSelectedValue().getPersonNr());
+		        	}
+		        }
+		        
+		   }
+		
+		 
 	 }
 }
