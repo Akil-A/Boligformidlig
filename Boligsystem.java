7d699a4c46be362d343b730ab6b3e481b8eaad78
joakim1123 <joakimm89@gmail.com>
2014-03-19 15:19:29 +0100

    Update Boligsystem.java

--- Boligsystem.java
+++ Boligsystem.java
@@ -1,65 +1,145 @@
+import java.util.ArrayList;
+import java.util.ListIterator;
+
 public class Boligsystem
 {
-	private Personliste personer;
-	private Kontraktliste kontrakter;
+	private ArrayList<Person> personer;
+	private ArrayList<Kontrakt> kontrakter;
 	
-	public Boligsystem(Personliste p, Kontraktliste k)
+	public Boligsystem(ArrayList<Person> p, ArrayList<Kontrakt> k)
 	{
 		personer = p;
 		kontrakter = k;
 	}
 	
+	
+	
+	// ###############################################################################################
+	// PERSON-METODER
+	// ###############################################################################################
+	
 	public Person finnPerson( int personNr )
 	{
-		return personer.finnPerson( personNr );
-	}
-	
-	public Bolig finnBolig( int boligNr )
-	{
-		return personer.finnBolig( boligNr );
-	}
-	
-	public Boligliste sokBoliger( String kriterier )
-	{
-		return personer.sokBoliger( kriterier );
-	}
-	
-	public Kontrakt finnKontrakt( int kontraktNr )
-	{
-		return kontrakter.finnKontrakt( kontraktNr );
+		ListIterator<Person> iter = personer.listIterator();
+		
+		while(iter.hasNext())
+			if(iter.next().getPersonNr() == personNr)
+				return iter.next();
+		
+		return null;
 	}
 	
 	public void settInnPerson(Person p)
 	{
-		personer.settInnPerson(p);
+		personer.add(p);
 	}
 
-	// indeks-parametern må gjøres om til personNr i Personliste-klassen
-	public void slettPerson(int indeks)
+	public Person slettPerson(int personNr)
 	{
-		personer.slettPerson(indeks);
+		Person pers = null;
+		
+		for (Person p : personer)
+			if (p.getPersonNr() == personNr)
+			{
+				pers = p;
+				personer.remove(p);
+			}
+		
+		return pers;
 	}
 	
-	
-	/* Metodene nedenfor, trengs de? Når man lagrer til og henter fra fil?
-	 ===================================== */
-	public Personliste getPersoner()
+	public ArrayList<Person> getPersoner()
 	{
 		return personer;
 	}
 	
-	public void setPersoner(Personliste p)
+	public ArrayList<Person> getBoligsokere()
 	{
-		personer = p;
+		ArrayList<Person> pl = new ArrayList<>();
+		
+		for (Person p : personer)
+			if (p instanceof Boligsoker)
+				pl.add(p);
+		
+		return pl;
 	}
 	
-	public Kontraktliste getKontrakter()
+	public ArrayList<Utleier> getUtleiere()
+	{
+		ArrayList<Utleier> pl = new ArrayList<>();
+		
+		for (Person p : personer)
+			if (p instanceof Utleier)
+				pl.add((Utleier) p);
+		
+		return pl;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	// ###############################################################################################
+	// BOLIG-METODER
+	// ###############################################################################################
+	
+	public Bolig finnBolig( int boligNr )
+	{
+		for (Utleier u : getUtleiere())
+			for (Bolig b : u.getBoliger())
+				if (b.getBoligNr() == boligNr)
+					return b;
+		
+		return null;
+	}
+	
+	public ArrayList<Bolig> sokBoliger( String kriterier )
+	{
+		// loop gjennom Utleiere, søk gjennom boliglistene deres etter boliger som matcher kriteriene
+		
+		return null;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	// ###############################################################################################
+	// KONTRAKT-METODER
+	// ###############################################################################################
+
+	
+	public void settInnKontrakt(Kontrakt k)
+	{
+		kontrakter.add(k);
+	}	
+	
+	public Kontrakt finnKontrakt( int kontraktNr )
+	{
+		ListIterator<Kontrakt> iter = kontrakter.listIterator();
+		
+		while(iter.hasNext())
+			if(iter.next().getKontraktNr() == kontraktNr)
+				return iter.next();
+		
+		return null;
+	}	
+	
+	public ArrayList<Kontrakt> getKontrakter()
 	{
 		return kontrakter;
 	}
 	
-	public void setKontrakter(Kontraktliste k)
+	// parametren må byttes til kontraktNr
+	public void slettKontrakt(int indeks)
 	{
-		kontrakter = k;
+		kontrakter.remove(indeks);
 	}
 }
