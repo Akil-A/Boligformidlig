8679974f674985d8031de50526419f35979e210a
joakim1123 <joakimm89@gmail.com>
2014-03-19 01:09:15 +0100

    Update Boligsoker.java

--- Boligsoker.java
+++ Boligsoker.java
@@ -1,6 +1,6 @@
 
-public class Boligsoker extends Person {
-	
+public class Boligsoker extends Person
+{
 	private int antallPersoner;
 	private String sivilstatus;
 	private String yrke;
@@ -12,99 +12,108 @@ public class Boligsoker extends Person {
 	private String byggeaar;
 	private String utleiepris;
 	
-	public Boligsoker(int antallPersoner, String sivilstatus, String yrke, String arbeidsforhold, boolean royker,boolean husdyr, int boareal, int antallRom, String byggeaar, String utleiepris, String navn, String adresse, String email, String telefon)
+	public Boligsoker(String fornavn, String etternavn, String adresse, int postnr, String poststed, String email, String telefon)
 	{
-		super(navn,adresse,email,telefon);
-		
-		this.antallPersoner = antallPersoner;
-		this.sivilstatus = sivilstatus;
-		this.yrke = yrke;
-		this.arbeidsforhold = arbeidsforhold;
-		this.royker = royker;
-		this.husdyr = husdyr;
-		this.boareal = boareal;
-		this.antallRom = antallRom;
-		this.byggeaar = byggeaar;
-		this.utleiepris = utleiepris;
+		super(fornavn, etternavn, adresse, postnr, poststed, email, telefon);
 	}
 
-	public int getAntallPersoner() {
+	public int getAntallPersoner()
+	{
 		return antallPersoner;
 	}
 
-	public void setAntallPersoner(int antallPersoner) {
+	public void setAntallPersoner(int antallPersoner)
+	{
 		this.antallPersoner = antallPersoner;
 	}
 
-	public String getSivilstatus() {
+	public String getSivilstatus()
+	{
 		return sivilstatus;
 	}
 
-	public void setSivilstatus(String sivilstatus) {
+	public void setSivilstatus(String sivilstatus)
+	{
 		this.sivilstatus = sivilstatus;
 	}
 
-	public String getYrke() {
+	public String getYrke()
+	{
 		return yrke;
 	}
 
-	public void setYrke(String yrke) {
+	public void setYrke(String yrke)
+	{
 		this.yrke = yrke;
 	}
 
-	public String getArbeidsforhold() {
+	public String getArbeidsforhold()
+	{
 		return arbeidsforhold;
 	}
 
-	public void setArbeidsforhold(String arbeidsforhold) {
+	public void setArbeidsforhold(String arbeidsforhold)
+	{
 		this.arbeidsforhold = arbeidsforhold;
 	}
 
-	public boolean isRoyker() {
+	public boolean isRoyker()
+	{
 		return royker;
 	}
 
-	public void setRoyker(boolean royker) {
+	public void setRoyker(boolean royker)
+	{
 		this.royker = royker;
 	}
 
-	public boolean isHusdyr() {
+	public boolean isHusdyr()
+	{
 		return husdyr;
 	}
 
-	public void setHusdyr(boolean husdyr) {
+	public void setHusdyr(boolean husdyr)
+	{
 		this.husdyr = husdyr;
 	}
 
-	public int getBoareal() {
+	public int getBoareal()
+	{
 		return boareal;
 	}
 
-	public void setBoareal(int boareal) {
+	public void setBoareal(int boareal)
+	{
 		this.boareal = boareal;
 	}
 
-	public int getAntallRom() {
+	public int getAntallRom()
+	{
 		return antallRom;
 	}
 
-	public void setAntallRom(int antallRom) {
+	public void setAntallRom(int antallRom)
+	{
 		this.antallRom = antallRom;
 	}
 
-	public String getByggeaar() {
+	public String getByggeaar()
+	{
 		return byggeaar;
 	}
 
-	public void setByggeaar(String byggeaar) {
+	public void setByggeaar(String byggeaar)
+	{
 		this.byggeaar = byggeaar;
 	}
 
-	public String getUtleiepris() {
+	public String getUtleiepris()
+	{
 		return utleiepris;
 	}
 
-	public void setUtleiepris(String utleiepris) {
+	public void setUtleiepris(String utleiepris)
+	{
 		this.utleiepris = utleiepris;
 	}
 }
