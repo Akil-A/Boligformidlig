3ae92ba01b4f657b16a62d39428964089e89d4e0
joakim1123 <joakimm89@gmail.com>
2014-03-19 17:03:39 +0100

    Create Interesse.java

---
+++ Interesse.java
@@ -0,0 +1,35 @@
+/*
+ * Klasse som forteller hvilken boligsoker som er interessert i hvilken bolig.
+ */
+
+public class Interesse
+{
+	private int interesseNr;
+	private static int interesseTeller = 0;
+	
+	private int boligsokerNr;
+	private int boligNr;
+	
+	public Interesse(int boligsokerNr, int boligNr)
+	{
+		interesseNr = interesseTeller++;
+		
+		this.boligsokerNr = boligsokerNr;
+		this.boligNr = boligNr;
+	}
+	
+	public int getInteresseNr()
+	{
+		return interesseNr;
+	}
+	
+	public int getBoligsokerNr()
+	{
+		return boligsokerNr;
+	}
+	
+	public int getBoligNr()
+	{
+		return boligNr;
+	}
+}
