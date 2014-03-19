a6d33793be1138a49f2d14756e290e1e91844a9e
joakim1123 <joakimm89@gmail.com>
2014-03-19 01:13:17 +0100

    Update Utleier.java

--- Utleier.java
+++ Utleier.java
@@ -1,32 +1,32 @@
 public class Utleier extends Person 
 {
 
-private String firma;
-private Boligliste boligliste;
-
-public Utleier (String fornavn, String etternavn, String adresse, int postnr, String poststed, String email, String telefon, String firma)
-{
-	super(fornavn, etternavn, adresse, postnr, poststed, email, telefon);
-	boligliste = new Boligliste();
-}
-
-public String getFirma() 
-{
-	return firma;
-}
-
-public void setFirma(String firma) 
-{
-	this.firma = firma;
-}
-
-public Boligliste getBoligliste() 
-{
-	return boligliste;
-}
-
-public void setBoligliste(Boligliste boligliste) 
-{
-	this.boligliste = boligliste;
-}
+	private String firma;
+	private Boligliste boligliste;
+	
+	public Utleier (String fornavn, String etternavn, String adresse, int postnr, String poststed, String email, String telefon)
+	{
+		super(fornavn, etternavn, adresse, postnr, poststed, email, telefon);
+		boligliste = new Boligliste();
+	}
+	
+	public String getFirma() 
+	{
+		return firma;
+	}
+	
+	public void setFirma(String firma) 
+	{
+		this.firma = firma;
+	}
+	
+	public Boligliste getBoligliste() 
+	{
+		return boligliste;
+	}
+	
+	public void setBoligliste(Boligliste boligliste) 
+	{
+		this.boligliste = boligliste;
+	}
 }
