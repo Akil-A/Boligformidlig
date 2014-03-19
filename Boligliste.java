9e24be326dd1f8b54961e275103e6b40c343e4a9
joakim1123 <joakimm89@gmail.com>
2014-03-19 01:08:42 +0100

    Update Boligliste.java

--- Boligliste.java
+++ Boligliste.java
@@ -2,19 +2,37 @@ import java.util.*;
 
 public class Boligliste
 {
-	List<Bolig> boliger = new ArrayList<>();
+	List<Bolig> boliger;
+	
+	public Boligliste()
+	{
+		boliger = new ArrayList<>();
+	}
 	
 	public void settInnBolig(Bolig b)
 	{
 		boliger.add(b);
 	}
 	
-	public void fjernBolig(int indeks)
+	public void slettBolig(int indeks)
 	{
 		boliger.remove(indeks);
 	}
 	
-	public Boligliste finnBolig(String adresse, int boareal, int antrom, String byggeaar, int utleiepris,
+	public Bolig finnBolig(int boligNr)
+	{
+		ListIterator<Bolig> iter = boliger.listIterator();
+		
+		while(iter.hasNext())
+			if(iter.next().getBoligNr() == boligNr)
+				return iter.next();
+		
+		return null;
+	}
+	
+	/* SØKEFUNKSJON -- denne er lang og kronglete, hva kan vi gjøre?
+	 =============================================================================================================== */
+	public Boligliste sokBolig(String adresse, int boareal, int antrom, String byggeaar, int utleiepris,
 			int antetasjer, boolean kjeller, int tomtestr,
 			int etasje, boolean heis, boolean balkong)
 	{
@@ -26,7 +44,7 @@ public class Boligliste
 		{
 			if (iter.next().getAdresse().equals(adresse) || iter.next().getBoareal() == boareal || iter.next().getAntrom() == antrom ||
 					iter.next().getByggeaar().equals(byggeaar) || iter.next().getUtleiepris() == utleiepris ||
-					((iter instanceof Enebolig || iter instanceof Rekkehus) && (((Enebolig)iter.next()).getAntetasjer() == antetasjer || ((Enebolig)iter.next()).getKjeller() == kjeller || ((Enebolig)iter.next()).getTomtestr() == tomtestr)) ||
+					((iter instanceof Enebolig || iter instanceof Rekkehus) && (((Enebolig)iter.next()).getAntetasjer() == antetasjer || ((Enebolig)iter.next()).isKjeller() == kjeller || ((Enebolig)iter.next()).getTomtestr() == tomtestr)) ||
 					(iter instanceof Leilighet && (((Leilighet)iter.next()).getEtasje() == etasje || ((Leilighet)iter.next()).isHeis() == heis || ((Leilighet)iter.next()).isBalkong() == balkong)))
 				bl.settInnBolig(iter.next());
 			else
@@ -36,6 +54,8 @@ public class Boligliste
 		return bl;
 	}
 	
+	
+	
 	public String visListe()
 	{
 		Iterator<Bolig> iter = boliger.iterator();
@@ -43,9 +63,7 @@ public class Boligliste
 		String s = "";
 		
 		while (iter.hasNext())
-		{
 			s += iter.next().toString() + "\n";
-		}
 		
 		return s;
 	}
