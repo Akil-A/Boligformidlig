ab8e45df5ec25006ee753870bfa5f74b4ca39be1
joakim1123 <joakimm89@gmail.com>
2014-03-19 01:13:01 +0100

    Update Rekkehus.java

--- Rekkehus.java
+++ Rekkehus.java
@@ -1,27 +1,24 @@
 public class Rekkehus extends Bolig
 {
-		private int antetasjer;
-		private boolean kjeller;
-		private int tomtestr;
+	private int antetasjer;
+	private boolean kjeller;
+	private int tomtestr;
 	
 	public Rekkehus(int antetasjer, boolean kjeller, int tomtestr,String adresse, int postnr, String poststed, int boareal, int antrom,String byggeaar,String beskrivelse, int utleiepris, int aar, int maaned, int dag)
 	{
+		super(adresse, postnr, poststed, boareal, antrom, byggeaar, beskrivelse, utleiepris);	
 	
-		super(adresse, postnr, poststed, boareal,antrom,byggeaar,beskrivelse,utleiepris,aar,maaned,dag);	
-	
-			this.antetasjer = antetasjer;
-			this.kjeller = kjeller;
-			this.tomtestr = tomtestr;
+		this.antetasjer = antetasjer;
+		this.kjeller = kjeller;
+		this.tomtestr = tomtestr;
 	}
 	
 	public int getAntetasjer() 
-	
 	{
 		return antetasjer;
 	}
 	
 	public void setAntetasjer(int antetasjer) 
-	
 	{
 		this.antetasjer = antetasjer;
 	}
