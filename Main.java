c4d365de189925ecd54aeda8eca452ccb82764eb
Akil-A <mail@asghara.com>
2014-03-19 14:26:59 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main 
+{
+		public static void main(String[] args)
+		{	
+			//Kontrakt ko = new Kontrakt();
+			//Kontraktliste klo = new Kontraktliste();
+			//Personliste plo = new Personliste();
+			//Boligsystem bso = new Personvindu();
+			Personvindu pvo = new Personvindu();
+		}
+}
