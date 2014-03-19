709d221dbf6b8d77846023d0f2a591ba2736c009
joakim1123 <joakimm89@gmail.com>
2014-03-19 01:12:44 +0100

    Update Personvindu.java

--- Personvindu.java
+++ Personvindu.java
@@ -2,27 +2,27 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 	
-public class Personvindu {
+public class Personvindu extends JFrame
+{
 
 	private JLabel felttekst1;
 	private JLabel felttekst2;
 
 	public Personvindu()
 	{	
-	  super("Personvindu");
-	  	
-	  	felttekst1 = new JLabel("Utleiere");
-	  	felttekst2 = new JLabel("Boligsokere");
-
-
-	    Container c = getContentPane();
-	    c.setLayout( new FlowLayout() );
-
-	    c.add( felttekst1);
-	    c.add( felttekst2);
-
-	    setSize( 420, 800 );
-	    setVisible( true );
-	    setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);   
-	  }
+		super("Personvindu");
+		
+		felttekst1 = new JLabel("Utleiere");
+		felttekst2 = new JLabel("Boligsokere");
+		
+		Container c = getContentPane();
+		c.setLayout( new FlowLayout() );
+		
+		c.add( felttekst1);
+		c.add( felttekst2);
+		
+		setSize( 420, 800 );
+		setVisible( true );
+		setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);   
+	 }
 }
