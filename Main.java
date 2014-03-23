80bc19e4df1c92e7d405446c9f4d0351a3e774f6
Akil-A <mail@asghara.com>
2014-03-23 19:06:58 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
-public class Main 
+import java.util.ArrayList;
+
+public class Main
 {
-		public static void main(String[] args)
-		{	
-			//Kontrakt ko = new Kontrakt();
-			//Kontraktliste klo = new Kontraktliste();
-			//Personliste plo = new Personliste();
-			//Boligsystem bso = new Personvindu();
-			Personvindu pvo = new Personvindu();
-		}
+    public static void main(String[] args)
+    {
+        //Kontrakt ko = new Kontrakt();
+        //Kontraktliste klo = new Kontraktliste();
+        //Personliste plo = new Personliste();
+        ArrayList<Person> p = new ArrayList<>();
+        ArrayList<Kontrakt> k = new ArrayList<>();
+        ArrayList<Interesse> i = new ArrayList<>();
+        Boligregister bso = new Boligregister(p, k, i);
+        Personvindu pvo = new Personvindu(bso);
+    }
 }
