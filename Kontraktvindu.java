09158aa9e53c64dda8b0b05f9e2a554201b1e9cc
joakim1123 <joakimm89@gmail.com>
2014-03-24 16:05:57 +0100

    Update Kontraktvindu.java

--- Kontraktvindu.java
+++ Kontraktvindu.java
@@ -2,6 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.event.*;
 
 
@@ -14,9 +15,7 @@ public class Kontraktvindu extends JFrame
 	public Kontraktvindu(Boligregister br)
 	{
 		super("Kontrakter");
-		
-		setSize( 500, 300 ); // bredde, høyde
-		
+				
 		lytter = new Lytter();
 		
 		fungerendeListe = new JList<>();
@@ -24,7 +23,14 @@ public class Kontraktvindu extends JFrame
 		
 		String[] dyrenavn = { "Fugl", "Katt", "Hund", "Kanin", "Gris" };
 		
+		Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
+		Font font = new Font("System", Font.PLAIN, 12);
+		
+		fungerendeListe.setBorder(border);
+		fungerendeListe.setFont(font);
 		fungerendeListe.setListData(dyrenavn);
+		utgaatteListe.setBorder(border);
+		utgaatteListe.setFont(font);
 		utgaatteListe.setListData(dyrenavn);
 
 		fungerendeKnapp = new JButton("Detaljer");
@@ -34,15 +40,44 @@ public class Kontraktvindu extends JFrame
 		utgaatteKnapp.addActionListener(lytter);
 		
 		Container c = getContentPane();
-		c.setLayout( new FlowLayout() );
+		c.setLayout( new GridBagLayout() );
 		
-		c.add(fungerendeListe);
-		c.add(fungerendeKnapp);
-		c.add(utgaatteListe);
-		c.add(utgaatteKnapp);
+		
+		fungerendeListe.setPreferredSize(new Dimension(450, (int) fungerendeListe.getPreferredSize().getHeight()));
+		utgaatteListe.setPreferredSize(new Dimension(450, (int) fungerendeListe.getPreferredSize().getHeight()));
+		
+		GridBagConstraints gc = new GridBagConstraints();
+		gc.anchor = GridBagConstraints.NORTHWEST;
+		
+		
+		gc.gridx = 0;
+		gc.gridy = 0;
+		c.add(new JLabel("Fungerende kontrakter:"), gc);
+		
+		gc.gridy = 1;
+		c.add(fungerendeListe, gc);
+		
+		gc.gridx = 1;
+		gc.insets.left = 10;
+		c.add(fungerendeKnapp, gc);
+		
+		gc.insets.top = 20;
+		gc.gridx = 0;
+		gc.gridy = 2;
+		gc.insets.left = 0;
+		c.add(new JLabel("Utgåtte kontrakter:"), gc);
+		
+		gc.gridy = 3;
+		gc.insets.top = 0;
+		c.add(utgaatteListe, gc);
+
+		gc.gridx = 1;
+		gc.insets.left = 10;
+		c.add(utgaatteKnapp, gc);
 
 		setVisible( true );
 		setLocationRelativeTo( null ); // Vinduet åpnes på midten av skjermen.
+		setSize( 600, 300 ); // bredde, høyde
 	}
 	
 	public static void main(String[] args)
