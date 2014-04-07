655427de088a7bf78158884a482197494fd56ebe
joakim1123 <joakim1123@users.noreply.github.com>
2014-04-07 13:55:09 +0200

    Update Hovedvindu.java

--- Hovedvindu.java
+++ Hovedvindu.java
@@ -15,6 +15,28 @@ public class Hovedvindu extends JFrame
 		
 		// boligregister må hentes fra fil
 		br = new Boligregister(new ArrayList<Person>(), new ArrayList<Kontrakt>(), new ArrayList<Interesse>());
+
+		/***** testdata *****/
+		Utleier p = new Utleier("Per", "Hansen", "Kirkegata 6", 3024, "Drammen", "", "");
+		Utleier p2 = new Utleier("Henrik", "Pettersen", "Avisveien 8", 3027, "Drammen", "", "");
+		
+		Rekkehus r = new Rekkehus("Borggata 12", 3027, "Drammen", 6000);
+		r.setBoareal(30);
+		r.setAntrom(5);
+		r.setByggeaar(2006);
+		r.setBeskrivelse("Hyggelig 3-roms med heis");
+		
+		Enebolig e = new Enebolig("Parkveien 16", 3024, "Drammen", 7000);
+		e.setBoareal(30);
+		e.setAntrom(5);
+		e.setByggeaar(2005);
+		e.setBeskrivelse("Nyoppusset og sentral beliggenhet");
+		
+		p.settInnBolig(e);
+		p2.settInnBolig(r);
+		br.settInnPerson(p2);
+		br.settInnPerson(p);
+		/***** testdata slutt *****/
 		
 		JTabbedPane tabbedPane = new JTabbedPane();
 		
