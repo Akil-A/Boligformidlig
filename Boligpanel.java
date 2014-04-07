4b65a7d893dbb769697993b6345b1c372fefb85f
joakim1123 <joakim1123@users.noreply.github.com>
2014-04-07 13:41:58 +0200

    Update Boligpanel.java

--- Boligpanel.java
+++ Boligpanel.java
@@ -248,8 +248,19 @@ public class Boligpanel extends JPanel
 		/***** testdata *****/
 		Utleier p = new Utleier("Per", "Hansen", "Kirkegata 6", 3024, "Drammen", "", "");
 		Utleier p2 = new Utleier("Henrik", "Pettersen", "Avisveien 8", 3027, "Drammen", "", "");
-		Rekkehus r = new Rekkehus("Borggata 12", 3027, "Drammen", 30 , 5 , 2006, "Hyggelig 3-roms med heis" ,6000);
-		Enebolig e = new Enebolig("Parkveien 16", 3024, "Drammen", 30, 5, 2005, "Nyoppusset og sentral beliggenhet", 7000);
+		
+		Rekkehus r = new Rekkehus("Borggata 12", 3027, "Drammen", 6000);
+		r.setBoareal(30);
+		r.setAntrom(5);
+		r.setByggeaar(2006);
+		r.setBeskrivelse("Hyggelig 3-roms med heis");
+		
+		Enebolig e = new Enebolig("Parkveien 16", 3024, "Drammen", 7000);
+		e.setBoareal(30);
+		e.setAntrom(5);
+		e.setByggeaar(2005);
+		e.setBeskrivelse("Nyoppusset og sentral beliggenhet");
+		
 		p.settInnBolig(e);
 		p2.settInnBolig(r);
 		br.settInnPerson(p2);
