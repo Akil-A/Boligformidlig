b7662509dbbb59caa4fde20c44bb591c44d581c0
Joakim <joakim121@users.noreply.github.com>
2014-04-11 13:27:04 +0200

    Update Enebolig.java

--- Enebolig.java
+++ Enebolig.java
@@ -1,22 +1,27 @@
-public class Rekkehus extends Bolig
+public class Enebolig extends Bolig
 {
 	private int antetasjer;
 	private boolean kjeller;
 	private int tomtestr;
-	
-	public Rekkehus(String adresse, int postnr, String poststed, int utleiepris)
+
+	public Enebolig(String adresse, int postnr, String poststed, int utleiepris)
 	{
 		super(adresse, postnr, poststed, utleiepris);
 	}
 	
-	public int getAntetasjer() 
+	public void setAntetasjer(int antetasjer)
+	{
+		this.antetasjer = antetasjer;
+	}
+	public int getAntetasjer()
 	{
 		return antetasjer;
 	}
 	
-	public void setAntetasjer(int antetasjer) 
+	public void setKjeller(boolean kjeller)
+	
 	{
-		this.antetasjer = antetasjer;
+		this.kjeller = kjeller;
 	}
 	
 	public boolean isKjeller() 
@@ -25,32 +30,25 @@ public class Rekkehus extends Bolig
 		return kjeller;
 	}
 	
-	public void setKjeller(boolean kjeller) 
-	
-	{
-		this.kjeller = kjeller;
-	}
-	
-	public int getTomtestr() 
-	
-	{
-		return tomtestr;
-	}
-	
-	public void setTomtestr(int tomtestr) 
+	public void setTomtestr(int tomtestr)
 	
 	{
 		this.tomtestr = tomtestr;
 	}
 	
+	public int getTomtestr() 
+	{
+		return tomtestr;
+	}
+	
 	
 	public String toString()
-	
 	{
 		return super.toString() +
-			"\nAntall etasjer: " + antetasjer +
-			"\nKjeller: " + (kjeller ? "ja" : "nei") +
-			"\nTomtestr.: " + tomtestr + " kvm";
+				"\nAntall etasjer: " + antetasjer +
+				"\nKjeller: " + (kjeller ? "ja" : "nei") +
+				"\nTomtestr.: " + tomtestr + " kvm";
 	}
 
 }
+
