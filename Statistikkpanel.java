a24036fb13766e756fa3cdf73393029dde604530
Joakim <joakim121@users.noreply.github.com>
2014-04-11 16:18:40 +0200

    Update Statistikkpanel.java

--- Statistikkpanel.java
+++ Statistikkpanel.java
@@ -1,11 +1,27 @@
+import java.awt.*;
 import javax.swing.*;
 
 public class Statistikkpanel extends JPanel
 {
 	public Statistikkpanel(Boligregister br)
 	{
-		add(new JLabel("Her kommer statistikk:"));
-		add(new JLabel("Det er " + br.getBoliger().size() + " boliger for utleie for øyeblikket."));
-		add(new JLabel("Hvor mange leiekontrakter firmaet har formidlet hittil i år."));
+		setLayout(new GridBagLayout());
+		
+		GridBagConstraints gc = new GridBagConstraints();
+		gc.gridx = 0;
+		gc.gridy = 0;
+		
+		add(new JLabel("Her kommer statistikk:"), gc);
+		
+		gc.gridy = 1;
+		
+		int antBoligerTotalt = br.getBoliger().size();
+		int antUtleide = br.getKontrakter().size();
+		
+		add(new JLabel("Det er " + antBoligerTotalt + " ledige boliger for utleie. " + antUtleide + " boliger er leid ut."), gc);
+		
+		gc.gridy = 2;
+		
+		add(new JLabel("Hvor mange leiekontrakter har firmaet formidlet hittil i år?"), gc);
 	}
 }
