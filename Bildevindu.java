5f3d2ee859105c8d757a655e0c6550c5b581919f
Joakim <joakim121@users.noreply.github.com>
2014-04-24 16:16:38 +0200

    Update Bildevindu.java

--- Bildevindu.java
+++ Bildevindu.java
@@ -8,9 +8,13 @@ public class Bildevindu extends JFrame
 	public Bildevindu(BufferedImage original)
 	{
 		super("Stort bilde");
-		setSize(800, 600);
+		setExtendedState(MAXIMIZED_BOTH);
+		setVisible(true);
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		Container c = getContentPane();
+
+		final int MAKS_B = getWidth();
+		final int MAKS_H = getHeight();
 
 		int origB = original.getWidth();
 		int origH = original.getHeight();
@@ -18,23 +22,21 @@ public class Bildevindu extends JFrame
 		int nyB = origB;
 		int nyH = origH;
 		
-		if (origB > 800)
+		if (nyB > MAKS_B)
 		{
-			nyB = 800;
-			nyH = origH / (origB / nyB);
+			nyB = MAKS_B;
+			nyH = nyH / (origB / MAKS_B);
 		}
 		
-		if (nyH > 600)
+		if (nyH > MAKS_H)
 		{
-			nyH = 600;
-			nyB = nyB / (origH / nyH);
+			nyH = MAKS_H;
+			nyB = nyB / (nyH / MAKS_H);
 		}
 		
-		Image nyttBilde = original.getScaledInstance(nyB, nyH, BufferedImage.SCALE_SMOOTH);
+		Image nyttBilde = original.getScaledInstance(nyB, nyH, BufferedImage.SCALE_FAST);
 		
 		JLabel picLabel = new JLabel(new ImageIcon(nyttBilde));
 		c.add(picLabel);
-		setLocationRelativeTo(null);
-		setVisible(true);
 	}
 }
