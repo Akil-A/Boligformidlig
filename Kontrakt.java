b97b849233f74549811d3f393c2e6ac92d121218
A-Lee <s198734@stud.hioa.no>
2014-04-29 16:52:11 +0200

    Update Kontrakt.java

--- Kontrakt.java
+++ Kontrakt.java
@@ -6,7 +6,6 @@ public class Kontrakt implements Serializable
 	private int kontraktNr;
 	private static int kontraktTeller = 0;
 	
-	private int utleierNr;
 	private int leietakerNr; // leietaker er det samme som boligsoker
 	private int boligNr;
 	private Date startdato;
@@ -14,11 +13,10 @@ public class Kontrakt implements Serializable
 	private Date oppsagtDato;
 	private String oppsigelsesgrunn;
 	
-	public Kontrakt(int utleierNr, int leietakerNr, int boligNr, Date startdato, Date sluttdato)
+	public Kontrakt(int leietakerNr, int boligNr, Date startdato, Date sluttdato)
 	{
 		kontraktNr = kontraktTeller++;
 		
-		this.utleierNr = utleierNr;
 		this.leietakerNr = leietakerNr;
 		this.boligNr = boligNr;
 		this.startdato = startdato;
@@ -28,16 +26,6 @@ public class Kontrakt implements Serializable
 	public int getKontraktNr()
 	{
 		return kontraktNr;
-	}
-
-	public int getUtleierNr()
-	{
-		return utleierNr;
-	}
-
-	public void setUtleier(int utleierNr)
-	{
-		this.utleierNr = utleierNr;
 	}
 
 	public int getLeietakerNr()
