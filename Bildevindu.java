209ac4e6fa25e7183fbd6025b32ac5d730233280
Akil-A <mail@asghara.com>
2014-05-02 18:05:01 +0200

    Update Bildevindu.java

--- Bildevindu.java
+++ Bildevindu.java
@@ -17,30 +17,30 @@ public class Bildevindu extends JFrame
 		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		Container c = getContentPane();	
 
-			final int MAKS_B = Toolkit.getDefaultToolkit().getScreenSize().width;
-			final int MAKS_H = Toolkit.getDefaultToolkit().getScreenSize().height;
-	
-			int origB = original.getWidth();
-			int origH = original.getHeight();
-			
-			int nyB = origB;
-			int nyH = origH;
-			
-			if (nyB > MAKS_B)
-			{
-				nyB = MAKS_B;
-				nyH = nyH / (origB / MAKS_B);
-			}
-			
-			if (nyH > MAKS_H)
-			{
-				nyH = MAKS_H;
-				nyB = nyB / (nyH / MAKS_H);
-			}
-			
-			Image nyttBilde = original.getScaledInstance(nyB, nyH, BufferedImage.SCALE_FAST);
-	
-			JLabel picLabel = new JLabel(new ImageIcon(nyttBilde));
-			c.add(picLabel);
+		final int MAKS_B = Toolkit.getDefaultToolkit().getScreenSize().width;
+		final int MAKS_H = Toolkit.getDefaultToolkit().getScreenSize().height;
+
+		int origB = original.getWidth();
+		int origH = original.getHeight();
+		
+		int nyB = origB;
+		int nyH = origH;
+		
+		if (nyB > MAKS_B)
+		{
+			nyB = MAKS_B;
+			nyH = nyH / (origB / MAKS_B);
+		}
+		
+		if (nyH > MAKS_H)
+		{
+			nyH = MAKS_H;
+			nyB = nyB / (nyH / MAKS_H);
+		}
+		
+		Image nyttBilde = original.getScaledInstance(nyB, nyH, BufferedImage.SCALE_FAST);
+
+		JLabel picLabel = new JLabel(new ImageIcon(nyttBilde));
+		c.add(picLabel);
 	}
 }
