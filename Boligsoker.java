ab274a1da9cea564951c125077d017c2537d6adc
Joakim <joakim121@users.noreply.github.com>
2014-05-02 18:24:50 +0200

    Update Boligsoker.java

--- Boligsoker.java
+++ Boligsoker.java
@@ -7,20 +7,14 @@ public class Boligsoker extends Person
     private String arbeidsforhold;
     private boolean royker;
     private boolean husdyr;
-    private boolean hage;
-    private boolean heis;
-    private boolean balkong;
-    private boolean parkering;
-    private boolean enebolig;
-    private boolean rekkehus;
-    private boolean leilighet;
-    private int fraStorrelse;
-    private int tilStorrelse;
-    private int antallRom;
-    private String byggeaar;
-    private int utleiepris;
-    private String firma;
-    private String togst;
+    private boolean kravEnebolig;
+    private boolean kravRekkehus;
+    private boolean kravLeilighet;
+    private int kravArealFra;
+    private int kravArealTil;
+    private int kravMinAntRom;
+    private int kravMinByggeaar;
+    private int kravMaksPris;
     
     public Boligsoker(String fornavn, String etternavn, String adresse, int postnr, String poststed, String email, String telefon)
     {
@@ -67,7 +61,7 @@ public class Boligsoker extends Person
         this.arbeidsforhold = arbeidsforhold;
     }
 
-    public boolean isRoyker()
+    public boolean getRoyker()
     {
         return royker;
     }
@@ -77,7 +71,7 @@ public class Boligsoker extends Person
         this.royker = royker;
     }
 
-    public boolean isHusdyr()
+    public boolean getHusdyr()
     {
         return husdyr;
     }
@@ -87,130 +81,83 @@ public class Boligsoker extends Person
         this.husdyr = husdyr;
     }
 
-    public int getAntallRom()
-    {
-        return antallRom;
-    }
-
-    public void setAntallRom(int antallRom)
-    {
-        this.antallRom = antallRom;
-    }
-
-    public String getByggeaar()
-    {
-        return byggeaar;
-    }
-
-    public void setByggeaar(String byggeaar)
-    {
-        this.byggeaar = byggeaar;
-    }
-
-    public int getUtleiepris()
-    {
-        return utleiepris;
-    }
-
-    public void setUtleiepris(int utleiepris)
-    {
-        this.utleiepris = utleiepris;
-    }
-
-    public int getFraStorrelse()
-    {
-        return fraStorrelse;
-    }
-
-    public void setFraStorrelse(int fraStorrelse)
-    {
-        this.fraStorrelse = fraStorrelse;
-    }
-
-    public int getTilStorrelse()
-    {
-        return tilStorrelse;
-    }
-
-
-    public void setTilStorrelse(int tilStorrelse)
-    {
-        this.tilStorrelse = tilStorrelse;
-    }
-
-    public String getFirma()
-    {
-        return firma;
-    }
-
-    public void setFirma(String firma)
-    {
-        this.firma = firma;
-    }
-    
-    public String getTogst()
-    {
-        return togst;
-    }
-
-    public void setTogst (String t)
-    {
-        this.togst = t;
-    }
-
-	public boolean isHage() {
-		return hage;
+	public boolean getKravEnebolig()
+	{
+		return kravEnebolig;
 	}
 
-	public void setHage(boolean hage) {
-		this.hage = hage;
+	public void setKravEnebolig(boolean kravEnebolig)
+	{
+		this.kravEnebolig = kravEnebolig;
 	}
 
-	public boolean isHeis() {
-		return heis;
+	public boolean getKravRekkehus()
+	{
+		return kravRekkehus;
 	}
 
-	public void setHeis(boolean heis) {
-		this.heis = heis;
+	public void setKravRekkehus(boolean kravRekkehus)
+	{
+		this.kravRekkehus = kravRekkehus;
 	}
 
-	public boolean isParkering() {
-		return parkering;
+	public boolean getKravLeilighet()
+	{
+		return kravLeilighet;
 	}
 
-	public void setParkering(boolean parkering) {
-		this.parkering = parkering;
+	public void setKravLeilighet(boolean kravLeilighet)
+	{
+		this.kravLeilighet = kravLeilighet;
 	}
 
-	public boolean isBalkong() {
-		return balkong;
+	public int getKravArealFra()
+	{
+		return kravArealFra;
 	}
 
-	public void setBalkong(boolean balkong) {
-		this.balkong = balkong;
+	public void setKravArealFra(int kravArealFra)
+	{
+		this.kravArealFra = kravArealFra;
 	}
 
-	public boolean isEnebolig() {
-		return enebolig;
+	public int getKravArealTil()
+	{
+		return kravArealTil;
 	}
 
-	public void setEnebolig(boolean enebolig) {
-		this.enebolig = enebolig;
+	public void setKravArealTil(int kravArealTil)
+	{
+		this.kravArealTil = kravArealTil;
 	}
 
-	public boolean isRekkehus() {
-		return rekkehus;
+	public int getKravMinAntRom()
+	{
+		return kravMinAntRom;
 	}
 
-	public void setRekkehus(boolean rekkehus) {
-		this.rekkehus = rekkehus;
+	public void setKravMinAntRom(int kravMinAntRom)
+	{
+		this.kravMinAntRom = kravMinAntRom;
 	}
 
-	public boolean isLeilighet() {
-		return leilighet;
+	public int getKravMinByggeaar()
+	{
+		return kravMinByggeaar;
 	}
 
-	public void setLeilighet(boolean leilighet) {
-		this.leilighet = leilighet;
+	public void setKravMinByggeaar(int kravMinByggeaar)
+	{
+		this.kravMinByggeaar = kravMinByggeaar;
+	}
+
+	public int getKravMaksUtleiepris()
+	{
+		return kravMaksPris;
+	}
+
+	public void setKravMaksUtleiepris(int kravMaksPris)
+	{
+		this.kravMaksPris = kravMaksPris;
 	}
 }
