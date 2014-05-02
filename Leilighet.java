f7c8af05225b7fe7f8936f7f3dd3db869155801e
Joakim <joakim121@users.noreply.github.com>
2014-05-02 18:30:10 +0200

    Update Leilighet.java

--- Leilighet.java
+++ Leilighet.java
@@ -6,7 +6,7 @@ public class Leilighet extends Bolig implements Serializable
 	 private Boolean garasje;
 	 private Boolean vaskeri;
 	 
-	 public Leilighet(String adresse, int postnr, String poststed, int utleiepris)
+	 public Leilighet(String adresse, String postnr, String poststed, int utleiepris)
 	 {
 		super(adresse, postnr, poststed, utleiepris);
 	 }
