258c505d6b695368a707a2de05710d21aed651dc
Akil-A <mail@asghara.com>
2014-05-06 15:10:00 +0200

    Update Statistikkpanel.java

--- Statistikkpanel.java
+++ Statistikkpanel.java
@@ -3,8 +3,13 @@ import javax.swing.*;
 
 public class Statistikkpanel extends JPanel
 {
+	private Boligregister register;
+	private int antallUtleide, antBoligerTotalt, antUtleide;
+	
 	public Statistikkpanel(Boligregister br)
-	{
+	{	
+		register = br;
+		
 		setLayout(new GridBagLayout());
 		
 		GridBagConstraints gc = new GridBagConstraints();
@@ -15,16 +20,23 @@ public class Statistikkpanel extends JPanel
 		
 		gc.gridy = 1;
 		
-		int antBoligerTotalt = br.getBoliger().size();
-		int antUtleide = br.getUtleide().size();
+		antBoligerTotalt = br.getBoliger().size();
+		antUtleide = br.getUtleide().size();
 		
 		add(new JLabel("Det er " + antBoligerTotalt + " ledige boliger for utleie. " + antUtleide + " boliger er leid ut."), gc);
 		
 		gc.gridy = 2;
 		
-		int antallUtleide = br.getUtleideiAAr();
+		antallUtleide = br.getUtleideiAAr();
 		
 		add(new JLabel("Det er " + antallUtleide + " leiekontrakter formidlet hittil i aar"), gc);
 		
+	}
+	
+	public void oppdaterStatistikk()
+	{
+		antBoligerTotalt = register.getBoliger().size();
+		antUtleide = register.getUtleide().size();
+		antallUtleide = register.getUtleideiAAr();
 	}
 }
