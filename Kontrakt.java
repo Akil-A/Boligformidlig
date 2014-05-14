44cf43d9bf8a4b9c8bb6db0a3fe367b2b772c24a
Joakim <joakim121@users.noreply.github.com>
2014-05-14 13:39:08 +0200

    Update Kontrakt.java

--- Kontrakt.java
+++ Kontrakt.java
@@ -1,4 +1,6 @@
 // Kontraktdata. Hvem som har leid hvilken bolig og når.
+// Laget av Joakim
+// Sist oppdatert 10/5
 
 import java.io.Serializable;
 import java.util.Calendar;
@@ -13,7 +15,9 @@ public class Kontrakt implements Serializable
 	private Calendar sluttdato;
 	private Calendar oppsigelsesdato;
 	private String oppsigelsesgrunn;
-	private boolean feilinntasting;
+	private boolean feilinntasting; 
+	/* boolean feilinntasting er ikke i bruk. kan settes til True hvis kontrakten sies opp fordi brukeren har tastet feil.
+	 * da kan man la være å regne den med i statistikken */
 	
 	public Kontrakt(Boligsoker leietaker, Bolig bolig, Calendar sluttdato)
 	{
