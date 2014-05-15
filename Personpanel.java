f343c9bd6c1e3e1900a458d72f4d4ad45b194caa
Akil-A <mail@asghara.com>
2014-05-15 19:41:44 +0200

    Update Personpanel.java

--- Personpanel.java
+++ Personpanel.java
@@ -13,18 +13,16 @@ public class Personpanel extends JPanel
 {
     private JList<Person> boligsokerliste, utleierliste;
     private DefaultListModel<Person> boligsokermodell, utleiermodell;
-    private Lytter lytter;
     private JButton utleierknapp, boligsokerknapp, nypersonknapp;
     private Boligregister register;
-	private Font IKKEFET = new Font(Font.SANS_SERIF, Font.PLAIN, 12);
 
     public Personpanel(Boligregister br)
     {
         register = br;
-        
+
 
         /********* DEFINERING AV KOMPONENTER START *********/
-        lytter = new Lytter();
+        Lytter lytter = new Lytter();
         utleierknapp = new JButton("Detaljer");
         utleierknapp.addActionListener(lytter);
         boligsokerknapp = new JButton("Detaljer");
@@ -33,57 +31,58 @@ public class Personpanel extends JPanel
         nypersonknapp.addActionListener(lytter);
         /********* DEFINERING AV KOMPONENTER SLUTT *********/
 
-        
+
         /********* POPULERING AV LISTER START *********/
-        utleiermodell = new DefaultListModel<Person>();
-        utleierliste = new JList<Person>(utleiermodell);
+        utleiermodell = new DefaultListModel<>();
+        utleierliste = new JList<>(utleiermodell);
+        Font IKKEFET = new Font(Font.SANS_SERIF, Font.PLAIN, 12);
         utleierliste.setFont(IKKEFET);
         oppdaterUtleierliste();
 
-        boligsokermodell = new DefaultListModel<Person>();
-        boligsokerliste = new JList<Person>(boligsokermodell);
+        boligsokermodell = new DefaultListModel<>();
+        boligsokerliste = new JList<>(boligsokermodell);
         boligsokerliste.setFont(IKKEFET);
         oppdaterBoligsokerliste();
         /********* POPULERING AV LISTER SLUTT *********/
-        
-        
+
+
         /********* LAYOUT START *********/
         setLayout(new GridBagLayout());
-		
-		GridBagConstraints gc = new GridBagConstraints();
-		gc.anchor = GridBagConstraints.NORTHWEST;
-		
-		gc.gridx = 0;
-		gc.gridy = 0;
-		
-		JScrollPane bScrollPane = new JScrollPane();
-		bScrollPane.setBorder(BorderFactory.createTitledBorder("<html>BOLIGS&Oslash;KERE</html>"));
-		bScrollPane.setViewportView(boligsokerliste);
-		bScrollPane.setPreferredSize(new Dimension(450, 200));   
+
+        GridBagConstraints gc = new GridBagConstraints();
+        gc.anchor = GridBagConstraints.NORTHWEST;
+
+        gc.gridx = 0;
+        gc.gridy = 0;
+
+        JScrollPane bScrollPane = new JScrollPane();
+        bScrollPane.setBorder(BorderFactory.createTitledBorder("<html>BOLIGS&Oslash;KERE</html>"));
+        bScrollPane.setViewportView(boligsokerliste);
+        bScrollPane.setPreferredSize(new Dimension(450, 200));
         add(bScrollPane, gc);
-		
-		gc.gridx = 1;
-		gc.insets.left = 10;
-		add(boligsokerknapp, gc);
-	
-		gc.insets.top = 20;
-		gc.gridx = 0;
-		gc.gridy = 1;
-		gc.insets.left = 0;
-		
-		JScrollPane uScrollPane = new JScrollPane();
-		uScrollPane.setBorder(BorderFactory.createTitledBorder("UTLEIERE"));
-		uScrollPane.setViewportView(utleierliste); 
-		uScrollPane.setPreferredSize(new Dimension(450, 200));   
+
+        gc.gridx = 1;
+        gc.insets.left = 10;
+        add(boligsokerknapp, gc);
+
+        gc.insets.top = 20;
+        gc.gridx = 0;
+        gc.gridy = 1;
+        gc.insets.left = 0;
+
+        JScrollPane uScrollPane = new JScrollPane();
+        uScrollPane.setBorder(BorderFactory.createTitledBorder("UTLEIERE"));
+        uScrollPane.setViewportView(utleierliste);
+        uScrollPane.setPreferredSize(new Dimension(450, 200));
         add(uScrollPane, gc);
 
-		gc.gridx = 1;
-		gc.insets.left = 10;
-		add(utleierknapp, gc);
-        
-		gc.gridy = 2;
+        gc.gridx = 1;
+        gc.insets.left = 10;
+        add(utleierknapp, gc);
+
+        gc.gridy = 2;
         gc.insets.top = 20;
-		gc.insets.left = 0;
+        gc.insets.left = 0;
         gc.gridwidth = 2;
         gc.gridx = 0;
         gc.anchor = GridBagConstraints.CENTER;
@@ -94,25 +93,25 @@ public class Personpanel extends JPanel
     // Oppdater listen over utleiere. 
     public void oppdaterUtleierliste()
     {
-    	utleiermodell.clear();
-    	
-    	Iterator<Utleier> iterator = register.getUtleiere().iterator();
-    	
+        utleiermodell.clear();
+
+        Iterator<Utleier> iterator = register.getUtleiere().iterator();
+
         while(iterator.hasNext())
-        	utleiermodell.addElement(iterator.next());
+            utleiermodell.addElement(iterator.next());
     }
-    
+
     // Oppdater listen over boligsOkere.
     public void oppdaterBoligsokerliste()
     {
-    	boligsokermodell.clear();
-    	
-    	Iterator<Boligsoker> iterator = register.getBoligsokere().iterator();
-    	
+        boligsokermodell.clear();
+
+        Iterator<Boligsoker> iterator = register.getBoligsokere().iterator();
+
         while(iterator.hasNext())
-        	boligsokermodell.addElement(iterator.next());
+            boligsokermodell.addElement(iterator.next());
     }
-    
+
     private class Lytter implements ActionListener
     {
         public void actionPerformed( ActionEvent e )
@@ -121,7 +120,7 @@ public class Personpanel extends JPanel
             {
                 if( utleierliste.getSelectedIndex() != -1)
                 {
-                	Person valgtperson = utleierliste.getSelectedValue();
+                    Person valgtperson = utleierliste.getSelectedValue();
                     new Personskjemavindu(register, Personpanel.this, valgtperson);
                 }
             }
@@ -129,8 +128,8 @@ public class Personpanel extends JPanel
             {
                 if( boligsokerliste.getSelectedIndex() != -1)
                 {
-                	Person valgtperson = boligsokerliste.getSelectedValue();                	
-                	new Personskjemavindu(register, Personpanel.this, valgtperson);
+                    Person valgtperson = boligsokerliste.getSelectedValue();
+                    new Personskjemavindu(register, Personpanel.this, valgtperson);
                 }
             }
             else if(e.getSource() == nypersonknapp)
